b4115fa0e51498c4c54579cf55af043733b7b2d4
Colin Strong <turvyc@gmail.com>
2016-02-22 18:39:40 -0800

    Added inverse DCT.

--- DCT.java
+++ DCT.java
@@ -50,6 +50,51 @@ public class DCT {
         return dctBlock;
     }
 
+    // Yes, there is a lot of code repetition here. I'm getting tired, ok?
+    public static PixelBlock idct(PixelBlock original) {
+        int[][] origY = original.getYChannel();
+        int[][] origU = original.getUChannel();
+        int[][] origV = original.getVChannel();
+
+        int[][] idctY = new int[PixelBlock.HEIGHT][PixelBlock.WIDTH];
+        int[][] idctU = new int[PixelBlock.HEIGHT][PixelBlock.WIDTH];
+        int[][] idctV = new int[PixelBlock.HEIGHT][PixelBlock.WIDTH];
+
+        // For each pixel in the block . . .
+        for (int u = 0; u < PixelBlock.HEIGHT; u++) {
+            for (int v = 0; v < PixelBlock.WIDTH; v++) {
+                double ySum = 0.0;
+                double uSum = 0.0;
+                double vSum = 0.0;
+
+                // The sums
+                for (int i = 0; i < PixelBlock.HEIGHT; i++) {
+                    for (int j = 0; j < PixelBlock.WIDTH; j++) {
+                        double cosine = C(u) * C(v) / 4 * 
+                            Math.cos(( (2 * i + 1) * u * Math.PI) / 16) * 
+                            Math.cos(( (2 * j + 1) * v * Math.PI) / 16);
+                        ySum += cosine * origY[i][j];
+                        uSum += cosine * origU[i][j];
+                        vSum += cosine * origV[i][j];
+                    }
+                }
+
+                
+                idctY[u][v] = (int) ySum;
+                idctU[u][v] = (int) uSum;
+                idctV[u][v] = (int) vSum;
+            }
+        }
+
+        PixelBlock idctBlock = new PixelBlock();
+
+        idctBlock.setChannel(PixelBlock.Y, idctY);
+        idctBlock.setChannel(PixelBlock.U, idctU);
+        idctBlock.setChannel(PixelBlock.V, idctV);
+
+        return idctBlock;
+    }
+
     private static double C(int i) {
         return (i == 0) ? Math.sqrt(2.0) / 2 : 1;
     }
